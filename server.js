911f0593f9592f4a3512a26846fcef1e3754ea89
Bunny Joel <bunnyjoel391@gmail.com>
2020-10-11 22:49:08 +1100

    no message

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const { createServer } = require('http')
-const { parse } = require('url')
 const next = require('next')
-
+const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+const { createServer } = require('http')
+
 
 app.prepare().then(() => {
   createServer((req, res) => {
@@ -20,8 +20,8 @@ app.prepare().then(() => {
     } else {
       handle(req, res, parsedUrl)
     }
-  }).listen(3000, (err) => {
+  }).listen(port, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log(`> Ready on http://localhost:${port}`);
   })
 })
